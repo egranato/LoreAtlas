16d14874a5ceeef84004c4c45a81d1390b456b15
Ethan Granato <ethan@granato.net>
2023-04-07 12:05:23 -0600

    minor deployment updates

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:3000',
   googleClientId:
     '225716183722-ur48f8nlofk6li42q64qvkt5lddvhceb.apps.googleusercontent.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.loreatlas.com/api',
+  apiUrl: 'https://api.loreatlas.com',
   googleClientId:
     '225716183722-ur48f8nlofk6li42q64qvkt5lddvhceb.apps.googleusercontent.com',
 };
